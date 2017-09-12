154a85b5e1137936ad8b32747b1e53683727018f
chmod-sorind <diaconescu.sorin.g@gmail.com>
2017-09-11 23:38:40 -0400

    collors are now changing based on planet.x position. the sun is expanding and contracting based on the position of the planet.x. Mapped planet.x to sun.diameter and wihite and black.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,21 @@
 var canv_w = window.innerWidth;
 var canv_h = window.innerHeight;
-
+var black_to_white = 0;
+var white_to_black = 255;
 
 var planet = {
     x : 0,
     y : canv_h / 2,
     z : 2,
-    diameter : 20
+    diameter : 20,
+    speed_right : 3,
+    speed_left : -3
 };
 
 var sun = {
     x : canv_w / 2,
     y : canv_h / 2,
-    diameter : 80
+    diameter : 0
 };
 
 function setup() {
@@ -20,44 +23,32 @@ function setup() {
 }
 
 function draw() {
+    black_to_white = map(planet.x, 0, canv_w, 0, 255);
+    white_to_black = map(planet.x, 0, canv_w, 255, 0);
     background(50);
     noStroke();
-    fill(255,150,0);
+
+    fill(white_to_black);
     ellipse(sun.x, sun.y, sun.diameter);
-    fill(0);
+
+    fill(black_to_white);
     ellipse(planet.x, planet.y, planet.diameter, planet.diameter);
 
-    if (planet.x < canv_w) {
-        planet.x = planet.x + 2
-        sun.diameter++;
-        // console.log("1. planet.x is: ", planet.x);
-        // console.log("1. window innerWidth is : ", canv_w);
-      } else if (planet.x == canv_w + 1) {
-            planet.x = 0;
-            sun.diameter = 80;
-            // console.log("2. planet.x is: ", planet.x);
-            // console.log("1. planet.y is: ", planet.y);
-        }
+    if (planet.x > canv_w) {
+        planet.speed = planet.speed_left;
+      } else if (planet.x == 0) {
+        planet.speed = planet.speed_right;
+      }
 
-    console.log("1. planet.y is: ", planet.y);
+    planet.x = planet.x + planet.speed;
+    sun.diameter = map(planet.x, 0, canv_w, 0, canv_h)
 
-    if (planet.y != (canv_h / 2) + 10) {
-        planet.y++;
-        console.log("2. planet.y is: ", planet.y);
-        // if (planet.y == (canv_h / 2) + 10) {
-        //     up_down = true;
-        //     console.log("2. up_down is: ", up_down);
-        // }
-    }
 
-    console.log("3. planet.y is: ", planet.y);
+      // else if (planet.x >= canv_w) {
+      //       planet.x = 0;
+      //       sun.diameter = 0;
+      //       // console.log("2. planet.x is: ", planet.x);
+      //       // console.log("1. planet.y is: ", planet.y);
+      //   }
 
-    if (planet.y != (canv_h / 2) - 10) {
-        planet.y = planet.y - 1;
-        console.log("4. planet.y is: ", planet.y);
-        // if (planet.y < (canv_h / 2) + 9) {
-        //     up_down = false;
-        //     console.log("5. up_down is: ", up_down);
-        // }
-    }
 }
